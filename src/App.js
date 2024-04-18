02def39030332017787418b8fd2a892e7ab4a815
Nischay <barejanischay5@gmail.com>
2024-04-18 11:18:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products.jsx';
+import ProductForm from './components/ProductForm.jsx';
+import { useState } from 'react';
 
 function App() {
+
+  const response = [
+    {
+      id: "p1",
+      title: "Maggie",
+      price: 10,
+      date: new Date(2023, 3, 17) //It's 17 april 2023 not 17 march 2023
+    },
+    {
+      id: "p2",
+      title: "Lays",
+      price: 20,
+      date: new Date(2023, 3, 15)
+    },
+    {
+      id: "p3",
+      title: "Kurkure",
+      price: 20,
+      date: new Date(2023, 1, 17)
+    },
+    {
+      id: "p4",
+      title: "Popcorn",
+      price: 80,
+      date: new Date(2023, 11, 17)
+    },
+  ]
+
+  const [data, setData] = useState(response);
+
+  // we passed this func as a prop to child component
+  function newProductHandler(newProduct) {
+    console.log("received ", newProduct, "from child component(ProductForm)");
+
+    // updating data
+    setData([...data, newProduct]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductForm newProductHandler={newProductHandler}></ProductForm>
+      <Products products={data}></Products>
     </div>
   );
 }
